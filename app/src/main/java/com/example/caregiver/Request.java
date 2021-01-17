d20bb85dae9bb95e9ada1b40b24e1a049dc354bf
minh <lenguyetminh1998@gmail.com>
2021-01-16 21:49:54 -0500

    Add in dialog box

--- app/src/main/java/com/example/caregiver/Request.java
+++ app/src/main/java/com/example/caregiver/Request.java
@@ -1,8 +1,11 @@
 package com.example.caregiver;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
 
 public class Request extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class Request extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_request);
+    }
+
+    public void openDialogBox(View v){
+        AlertDialog.Builder builder = new AlertDialog.Builder(Request.this);
+        builder.setMessage("Send email?").setPositiveButton("Send", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                System.out.println("Handling email request here");
+            }
+        }).setNegativeButton("Cancel", null);
+
+
+        AlertDialog alert = builder.create();
+        alert.show();
     }
 }
