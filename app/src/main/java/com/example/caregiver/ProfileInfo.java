8a7c5759c998b6509df1cf0a90dce2f6e8c823e2
Minh Le <lenguyetminh1998@gmail.com>
2021-01-27 17:18:17 -0500

    Add in function to change email and user

--- app/src/main/java/com/example/caregiver/ProfileInfo.java
+++ app/src/main/java/com/example/caregiver/ProfileInfo.java
@@ -1,14 +1,16 @@
 package com.example.caregiver;
 
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
-import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
 import android.preference.PreferenceManager;
 import android.text.Html;
+import android.text.InputType;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -34,12 +36,11 @@ import com.google.firebase.database.ValueEventListener;
  * Use the {@link ProfileInfo#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class ProfileInfo extends Fragment{
+public class ProfileInfo extends Fragment {
 
     // Variables pointing to the field names
     public EditText nameField;
     public EditText emailField;
-    public EditText oldPasswordField;
     public EditText newPasswordField;
     public EditText confirmPasswordField;
     public TextView errorMessage;
@@ -52,9 +53,20 @@ public class ProfileInfo extends Fragment{
     int red;
     int green;
 
+    public class ProfileUser {
+        public String name;
+        public String email;
+        public ProfileUser(String name, String email) {
+            this.name = name;
+            this.email = email;
+        }
+    }
+
     public ProfileInfo() {
         // Required empty public constructor
     }
+
+
 
     // TODO: Rename and change types and number of parameters
     public static ProfileInfo newInstance() {
@@ -72,7 +84,7 @@ public class ProfileInfo extends Fragment{
      * @param view the view of the profile info page
      * @param userId the user id of the currently logged in user
      */
-    public void displayUserInfo(View view, String userId){
+    public void displayUserInfo(View view, String userId) {
         DatabaseReference database = FirebaseDatabase.getInstance().getReference();
         DatabaseReference ref = database.child("users/" + userId);
 
@@ -111,10 +123,9 @@ public class ProfileInfo extends Fragment{
         updateButton.setOnClickListener(updateUserInfoListener);
         nameField = (EditText) view.findViewById(R.id.profileName);
         emailField = (EditText) view.findViewById(R.id.profileEmail);
-        oldPasswordField = (EditText) view.findViewById(R.id.profileOldPassword);
         newPasswordField = (EditText) view.findViewById(R.id.profileNewPassword);
         confirmPasswordField = (EditText) view.findViewById(R.id.profileNewPassword2);
-        errorMessage =  (TextView) view.findViewById(R.id.profileInfoMessage);
+        errorMessage = (TextView) view.findViewById(R.id.profileInfoMessage);
 
         // Set color
         red = view.getResources().getColor(R.color.red);
@@ -124,58 +135,121 @@ public class ProfileInfo extends Fragment{
     }
 
     /**
-     * Render the error message field.
-     * @param sourceString The text message to be displayed
+     * Render the error and success message field.
+     * @param sourceString The text message to be displayed.
+     * @param color The color for the text message (red for error, green for success).
      */
-    public void displayMessage(String sourceString, int color){
+    public void displayMessage(String sourceString, int color) {
         errorMessage.setText(Html.fromHtml(sourceString));
         errorMessage.setVisibility(View.VISIBLE);
         errorMessage.setTextColor(color);
     }
 
     /**
-     * This function will be called if user want to change password
-     * @param oldPassword User current password
+     * Handle user password update.
+     * @param user The current user who is logged in the app
      * @param newPassword User new password
      * @param confirmPassword User new password (should be same as newPassword)
      */
-    public void handleChangePassword(String oldPassword, String newPassword, String confirmPassword){
-        if (oldPassword.isEmpty() || oldPassword == null) {
-            displayMessage("You need to input your old password first.", red);
-            return;
-        }
-        if (!newPassword.equals(confirmPassword)){
+    public void changePassword(
+            FirebaseUser user, @NonNull String newPassword, @NonNull String confirmPassword) {
+        if (!newPassword.equals(confirmPassword)) {
             displayMessage("Your new and confirm password must be the same.", red);
             return;
         }
-        if (currentEmail == null || currentEmail.isEmpty()) {
-            displayMessage("Unable to get your current email", red);
+        if (newPassword.length() < 6 || confirmPassword.length() < 6) {
+            displayMessage("Your password must be longer than 6 characters", red);
             return;
         }
-
-        // Re-authenticate user to ensure they enter the correct old password
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        AuthCredential credential = EmailAuthProvider
-                .getCredential(currentEmail, oldPassword);
-        user.reauthenticate(credential).addOnCompleteListener(new OnCompleteListener<Void>() {
+        user.updatePassword(newPassword).addOnCompleteListener(new OnCompleteListener < Void > () {
             @Override
-            public void onComplete(@NonNull Task<Void> task) {
+            public void onComplete(@NonNull Task < Void > task) {
                 if (task.isSuccessful()) {
-                    user.updatePassword(newPassword).addOnCompleteListener(new OnCompleteListener<Void>() {
-                        @Override
-                        public void onComplete(@NonNull Task<Void> task) {
-                            if (task.isSuccessful()) {
-                                displayMessage("Successfully change your password", green);
-                            } else {
-                                displayMessage("Cannot update password.", red);
-                            }
-                        }
-                    });
+                    displayMessage("Successfully change your password", green);
                 } else {
-                    displayMessage("Your old password is not correct.", red);
+                    displayMessage("Cannot update password.", red);
                 }
             }
         });
+    }
+
+    /**
+     * Handle user email update.
+     * @param user The current user who is logged in the app
+     * @param email User new email
+     */
+    public void changeEmail(FirebaseUser user, @NonNull String email){
+        DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
+        user.updateEmail(email).addOnCompleteListener(new OnCompleteListener < Void > () {
+            @Override
+            public void onComplete(@NonNull Task < Void > task) {
+                if (task.isSuccessful()) {
+                    displayMessage("Successfully change your email", green);
+                    rootRef.child("users").child(user.getUid()).child("email").setValue(email);
+                } else {
+                    displayMessage("Cannot update email.", red);
+                }
+            }
+        });
+    }
+
+    /**
+     * User must be logged in and has enter their password before they can modify profile.
+     */
+    public void askForOldPassword() {
+        AlertDialog.Builder alert = new AlertDialog.Builder(getActivity());
+        alert.setMessage("Please input your old password below.");
+        final EditText input = new EditText(getActivity());
+        input.setInputType(InputType.TYPE_TEXT_VARIATION_PASSWORD);
+        alert.setView(input);
+        alert.setPositiveButton("Change Profile", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                String password = input.getText().toString();
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                AuthCredential credential = EmailAuthProvider.getCredential(currentEmail, password);
+                user.reauthenticate(credential).addOnCompleteListener(new OnCompleteListener < Void > () {
+                    @Override
+                    public void onComplete(@NonNull Task < Void > task) {
+                        if (task.isSuccessful()) {
+                            updateUserInformation(user);
+                            displayMessage("Your profile is updated!", green);
+                        } else {
+                            displayMessage("Your old password is not correct.", red);
+                        }
+                    }
+                });
+            }
+        });
+        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                return;
+            }
+        });
+        alert.show();
+    }
+
+    /**
+     * Updates user information after user has verified their old password.
+     * @param user The current logged in Firebase user
+     */
+    public void updateUserInformation(@NonNull FirebaseUser user) {
+
+        DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
+
+        String name = nameField.getText().toString();
+        String email = emailField.getText().toString();
+        String newPassword = newPasswordField.getText().toString();
+        String confirmPassword = confirmPasswordField.getText().toString();
+
+        if (name != null && !name.isEmpty()) {
+            rootRef.child("users").child(user.getUid()).child("name").setValue(name);
+        }
+        if (!email.isEmpty()) {
+            changeEmail(user, email);
+        }
+        if (!newPassword.isEmpty() && !confirmPassword.isEmpty()) {
+            changePassword(user, newPassword, confirmPassword);
+        }
     }
 
     /**
@@ -184,20 +258,7 @@ public class ProfileInfo extends Fragment{
     private View.OnClickListener updateUserInfoListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            String name = nameField.getText().toString();
-            String email = emailField.getText().toString();
-            String oldPassword = oldPasswordField.getText().toString();
-            String newPassword = newPasswordField.getText().toString();
-            String confirmPassword = confirmPasswordField.getText().toString();
-            if (newPassword != null && confirmPassword != null){
-                handleChangePassword(oldPassword, newPassword, confirmPassword);
-            }
-            if (name != null && !name.isEmpty()){
-                currentName = name;
-            }
-            if (email != null && !email.isEmpty()){
-                currentEmail = email;
-            }
+            askForOldPassword();
         }
     };
 
