b192d4f50a73b8ccb6323a635dafa90d66665d23
Minh Le <lenguyetminh1998@gmail.com>
2021-02-01 18:56:33 -0500

    Build template for tasks

---
+++ app/src/main/java/com/example/caregiver/MainAdapter.java
@@ -0,0 +1,95 @@
+package com.example.caregiver;
+
+import android.graphics.Typeface;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class MainAdapter extends BaseExpandableListAdapter {
+    // Initialize variable
+
+    ArrayList<String> listGroup;
+    HashMap<String,ArrayList<String>> listChild;
+
+    public MainAdapter(ArrayList<String> listGroup, HashMap<String,ArrayList<String>> listChild){
+        this.listGroup = listGroup;
+        this.listChild = listChild;
+    }
+
+    @Override
+    public int getGroupCount() {
+        return listGroup.size();
+    }
+
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        return listChild.get(listGroup.get(groupPosition)).size();
+    }
+
+    @Override
+    public Object getGroup(int groupPosition) {
+        return listGroup.get(groupPosition);
+    }
+
+    @Override
+    public Object getChild(int groupPosition, int childPosition) {
+        return listChild.get(listGroup.get(groupPosition)).get(childPosition);
+    }
+
+    @Override
+    public long getGroupId(int groupPosition) {
+        return groupPosition;
+    }
+
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return 0;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded, View view, ViewGroup viewGroup) {
+        view = LayoutInflater.from(viewGroup.getContext()).inflate(
+                android.R.layout.simple_expandable_list_item_1, viewGroup, false);
+        TextView textView = view.findViewById(android.R.id.text1);
+        String sGroup = String.valueOf(getGroup(groupPosition));
+        textView.setText(sGroup);
+        textView.setTypeface(null, Typeface.BOLD);
+        return view;
+    }
+
+    @Override
+    public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View view, ViewGroup viewGroup) {
+        view =  LayoutInflater.from(viewGroup.getContext()).inflate(
+                android.R.layout.simple_selectable_list_item, viewGroup, false);
+        TextView textView = view.findViewById(android.R.id.text1);
+        String sChild = String.valueOf(getChild(groupPosition, childPosition));
+        textView.setText(sChild);
+        textView.setTypeface(null, Typeface.BOLD);
+
+        // Set onclick listener
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(viewGroup.getContext(), sChild, Toast.LENGTH_SHORT).show();
+            }
+        });
+        return view;
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return false;
+    }
+}
--- app/src/main/java/com/example/caregiver/TaskFragment.java
+++ app/src/main/java/com/example/caregiver/TaskFragment.java
@@ -7,6 +7,11 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ExpandableListView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,14 +20,10 @@ import android.view.ViewGroup;
  */
 public class TaskFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    ExpandableListView caregiveeList;
+    ArrayList<String> listGroup = new ArrayList<>();
+    HashMap<String, ArrayList<String>> listChild = new HashMap<>();
+    MainAdapter adapter;
 
     public TaskFragment() {
         // Required empty public constructor
@@ -40,8 +41,7 @@ public class TaskFragment extends Fragment {
     public static TaskFragment newInstance(String param1, String param2) {
         TaskFragment fragment = new TaskFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+
         fragment.setArguments(args);
         return fragment;
     }
@@ -49,16 +49,29 @@ public class TaskFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_task, container, false);
+        View view = inflater.inflate(R.layout.fragment_task, container, false);
+
+        // Set up the child list view
+
+        caregiveeList = (ExpandableListView) view.findViewById(R.id.caregiveelist);
+        for (int g = 0; g <= 10; g++){
+            listGroup.add("Group" + g);
+            ArrayList<String> arrayList = new ArrayList<>();
+            for (int c = 0; c <= 10; c++){
+               arrayList.add("Item" + c);
+            }
+            listChild.put(listGroup.get(g), arrayList);
+        }
+
+        adapter = new MainAdapter(listGroup, listChild);
+        caregiveeList.setAdapter(adapter);
+        return view;
     }
 }
