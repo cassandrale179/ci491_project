a3ed159860e8e61949883af827102c5d8c4f0a90
juihanamshet <juihanamshet97@gmail.com>
2021-02-02 18:36:14 -0500

    added region information collection

--- app/src/main/java/com/example/caregiver/BeaconFragment.java
+++ app/src/main/java/com/example/caregiver/BeaconFragment.java
@@ -2,47 +2,55 @@ package com.example.caregiver;
 
 import android.content.ContentProviderClient;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.RecentlyNonNull;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 
+import android.text.Html;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.caregiver.services.BeaconScanService;
 
-/**
- * Start Scanning button 
- * 
- */
-
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link BeaconFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class BeaconFragment extends Fragment {
 
     // Variables pointing to field names
     public EditText UUIDField; //check
     public EditText regionNameField;
     public EditText majorField;
-    public EditText minorField; 
+    public EditText minorField;
 
-    public class regionInfo {
+
+    public static class regionInfo {
         public String UUID; 
         public String regionName;
-        public String majorValue;
-        public String minorValue;
+        public int majorValue;
+        public int minorValue;
 
-        public regionInfo(String UUID, String regionName, String majorValue, String minorValue){
+        public regionInfo(String UUID, String regionName, int majorValue, int minorValue){
             this.UUID = UUID;
             this.regionName = regionName;
             this.majorValue = majorValue;
             this.minorValue = minorValue;
+        }
+
+        @RecentlyNonNull
+        @Override
+        public String toString() {
+            return "regionInfo{" +
+                    "UUID='" + UUID + '\'' +
+                    ", regionName='" + regionName + '\'' +
+                    ", majorValue=" + majorValue +
+                    ", minorValue=" + minorValue +
+                    '}';
         }
     }
 
@@ -51,8 +59,7 @@ public class BeaconFragment extends Fragment {
     }
 
     public static BeaconFragment newInstance() {
-        BeaconFragment fragment = new BeaconFragment();
-        return fragment;
+        return new BeaconFragment();
     }
 
     @Override
@@ -60,30 +67,64 @@ public class BeaconFragment extends Fragment {
         super.onCreate(savedInstanceState);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_beacon, container, false);
-        Button startScanButton = (Button) rootView.findViewById(R.id.start_button);
         Intent scanServiceIntent = new Intent(getActivity(), BeaconScanService.class);
-        startScanButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                getActivity().startService(scanServiceIntent);
-            }
-        });
 
-        Button stopScanButton = (Button) rootView.findViewById(R.id.stop_button);
-        stopScanButton.setOnClickListener(new View.OnClickListener() {
+        Button startScanButton = rootView.findViewById(R.id.start_button);
+        startScanButton.setOnClickListener(v -> startBeaconScanService(scanServiceIntent));
 
-            @Override
-            public void onClick(View v) {
-                getActivity().stopService(scanServiceIntent);
-            }
-        });
+        Button stopScanButton = rootView.findViewById(R.id.stop_button);
+        stopScanButton.setOnClickListener(v -> stopBeaconScanService(scanServiceIntent));
+
+        Button addRegionButton = rootView.findViewById(R.id.add_region);
+        addRegionButton.setOnClickListener(v -> addRegion(rootView));
 
         return rootView;
+
+    }
+
+    public void startBeaconScanService(Intent scanServiceIntent){
+        getActivity().startService(scanServiceIntent);
+    }
+
+    public void stopBeaconScanService(Intent scanServiceIntent){
+        getActivity().stopService(scanServiceIntent);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void addRegion(View rootView){
+        UUIDField = rootView.findViewById(R.id.UUIDText);
+        String UUIDValue = UUIDField.getText().toString();
+
+        regionNameField = rootView.findViewById(R.id.regionName);
+        String regionName = regionNameField.getText().toString();
+
+        majorField = rootView.findViewById(R.id.major);
+        String majorValue = majorField.getText().toString();
+
+        minorField = rootView.findViewById(R.id.minor);
+        String minorValue = minorField.getText().toString();
+
+        if (UUIDValue.isEmpty() || regionName.isEmpty() || majorValue.isEmpty() || minorValue.isEmpty()){
+            displayErrorMessage("One or more fields are empty.", rootView);
+        }
+        else {
+            displayErrorMessage("", rootView);
+            regionInfo newRegionInfo = new regionInfo(UUIDValue, regionName, Integer.parseUnsignedInt(majorValue), Integer.parseUnsignedInt(minorValue));
+            Log.i("Sample", "region Info = " + newRegionInfo.toString());
+        }
+
+    }
+
+    public void displayErrorMessage(String sourceString, View rootView){
+        TextView textView = rootView.findViewById(R.id.addRegionMessage);
+        textView.setText(Html.fromHtml(sourceString));
+        textView.setVisibility(View.VISIBLE);
     }
 
 
