6ee50265236148e31ae5f8acddef71513569caa0
juihanamshet <juihanamshet97@gmail.com>
2021-02-05 11:07:30 -0500

    fixed multi dex errors

--- app/src/main/java/com/example/caregiver/MainActivity.java
+++ app/src/main/java/com/example/caregiver/MainActivity.java
@@ -1,40 +1,15 @@
 package com.example.caregiver;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.firebase.database.FirebaseDatabase;
-import com.kontakt.sdk.android.ble.configuration.ActivityCheckConfiguration;
-import com.kontakt.sdk.android.ble.configuration.ForceScanConfiguration;
-import com.kontakt.sdk.android.ble.configuration.ScanMode;
-import com.kontakt.sdk.android.ble.configuration.ScanPeriod;
-import com.kontakt.sdk.android.ble.connection.OnServiceReadyListener;
-import com.kontakt.sdk.android.ble.manager.ProximityManager;
-import com.kontakt.sdk.android.ble.manager.ProximityManagerFactory;
-import com.kontakt.sdk.android.ble.manager.listeners.EddystoneListener;
-import com.kontakt.sdk.android.ble.manager.listeners.IBeaconListener;
-import com.kontakt.sdk.android.ble.manager.listeners.simple.SimpleEddystoneListener;
-import com.kontakt.sdk.android.ble.manager.listeners.simple.SimpleIBeaconListener;
-import com.kontakt.sdk.android.ble.manager.listeners.simple.SimpleScanStatusListener;
-import com.kontakt.sdk.android.ble.rssi.RssiCalculators;
-import com.kontakt.sdk.android.ble.spec.EddystoneFrameType;
-import com.kontakt.sdk.android.common.KontaktSDK;
-import com.kontakt.sdk.android.common.profile.IBeaconDevice;
-import com.kontakt.sdk.android.common.profile.IBeaconRegion;
-import com.kontakt.sdk.android.common.profile.IEddystoneDevice;
-import com.kontakt.sdk.android.common.profile.IEddystoneNamespace;
-import com.example.caregiver.services.BeaconScanService;
 
-import java.util.Arrays;
-import java.util.concurrent.TimeUnit;
-import android.content.Intent;
-import android.os.Bundle;
 import android.view.View;
 
 
@@ -44,49 +19,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        bottomNavigationView = findViewById(R.id.bottomNavigation);
-
-        bottomNavigationView.setOnNavigationItemSelectedListener(bottomNavigationMethod);
-        getSupportFragmentManager().beginTransaction().replace(R.id.container, new HomeFragment()).commit();
-        
-        //TODO: How do we get API key from user?
-    }
-
-
-
-    private  BottomNavigationView.OnNavigationItemSelectedListener bottomNavigationMethod = new
-            BottomNavigationView.OnNavigationItemSelectedListener() {
-                @Override
-                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-
-                    Fragment fragment = null;
-
-                    switch(menuItem.getItemId()){
-
-                        case R.id.home:
-                            fragment = new HomeFragment();
-                            break;
-
-                        case R.id.task:
-                            fragment = new TaskFragment();
-                            break;
-
-                        case R.id.beacon:
-                            fragment = new BeaconFragment();
-                            break;
-
-                        case R.id.profile:
-                            fragment = new ProfileFragment();
-                            break;
-
-                    }
-
-                    getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
-
-                    return true;
-                }
-            };
     }
 
     /** Navigation function to move to Identification Screen **/
