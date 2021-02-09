930ec3f38ef587299edca90f36beee82bcfa092d
Minh Le <lenguyetminh1998@gmail.com>
2021-02-09 00:11:16 -0500

    Finish button

--- app/src/main/java/com/example/caregiver/TaskSingleView.java
+++ app/src/main/java/com/example/caregiver/TaskSingleView.java
@@ -2,7 +2,11 @@ package com.example.caregiver;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
+import android.text.Html;
+import android.widget.TextView;
 
 /**
  * This function is accessible to Caregivee only. It allows them to click on a Task and
@@ -15,6 +19,19 @@ public class TaskSingleView extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_single_view);
 
+        // Programtically set background color for the buttons
+        TextView playButton = findViewById(R.id.playButton);
+        GradientDrawable playBg = (GradientDrawable)playButton.getBackground();
+        playBg.setColor(getResources().getColor(R.color.teal_700));
+
+        TextView timer = findViewById(R.id.timer);
+        GradientDrawable timerBg = (GradientDrawable)timer.getBackground();
+        timerBg.setColor(getResources().getColor(R.color.black));
+
+        TextView helpBtn = findViewById(R.id.helpMe);
+        GradientDrawable helpBg = (GradientDrawable)helpBtn.getBackground();
+        helpBg.setColor(getResources().getColor(R.color.gray));
+        helpBtn.setTextColor(Color.BLACK);
 
     }
 }
