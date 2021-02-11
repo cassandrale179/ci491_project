3dd5bd98651a840efe75515200c138f356a2aeac
Saloni Purswani <sppurswani@gmail.com>
2021-02-11 17:58:41 -0500

    Added email verification to add caregivee request

--- app/src/main/java/com/example/caregiver/Request.java
+++ app/src/main/java/com/example/caregiver/Request.java
@@ -22,13 +22,11 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
-import java.util.ArrayList;
-import java.util.Formatter;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Request extends AppCompatActivity {
 
@@ -65,12 +63,51 @@ public class Request extends AppCompatActivity {
         // add request to caregivee
         Map<String, Object> map = new HashMap<>();
         // Get current userId
-        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        SharedPreferences preferences = PreferenceManager
+                .getDefaultSharedPreferences(getApplicationContext());
         String userId = preferences.getString("userId", "");
         String name = preferences.getString("name", "");
-        Log.d("SUCCESS", "sendFirebaseRequest:found userId " + userId + " and name " + name);
+        // update map & save to firebase
         map.put(caregiveeId + "/requests/" + userId, name);
         ref.updateChildren(map);
+        Log.d("SUCCESS", "sendFirebaseRequest:request sent to caregivee id " + caregiveeId);
+    }
+
+    // alert messages & navigate to dashboard
+    private void displayRequestSent(){
+        // inform user of sent request
+        String alertReqSent = "Request sent! Please wait for this request to be approved.";
+        AlertDialog.Builder builderReqSent = new AlertDialog.Builder(Request.this)
+                .setMessage(alertReqSent)
+                .setPositiveButton("Okay",(dialog, which) -> {
+                    // navigate to dashboard
+                    Intent i = new Intent(Request.this, Dashboard.class);
+                    startActivity(i);
+                });
+        builderReqSent.create().show();
+    }
+    // alert messages
+    private void displayNonExistingCaregivee(){
+        // display message of non existing user
+        String alertNonCaregivee = "It looks this user is not a registered Caregivee. " +
+                "Please enter a valid Caregivee's email.";
+        AlertDialog.Builder builderReqSent = new AlertDialog.Builder(Request.this)
+                .setMessage(alertNonCaregivee)
+                .setPositiveButton("Okay", null);
+        builderReqSent.create().show();
+    }
+
+    /**
+     * method is used for checking valid email id format.
+     *
+     * @param email
+     * @return boolean true for valid false for invalid
+     */
+    public static boolean isEmailValid(String email) {
+        String expression = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$";
+        Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
     }
 
     /**
@@ -82,39 +119,31 @@ public class Request extends AppCompatActivity {
         EditText emailField = findViewById(R.id.requestEmail);
         String email = emailField.getText().toString();
 
-        if (email.isEmpty()){
+        // check for valid email
+        if(!isEmailValid(email)){
             displayMessage("Please enter a valid email address", "#922B21");
         } else {
-
-            String alertMessage = "Would you like to ask " + email + " to be a Caregivee?";
+            String alertMessage = "Would you like to send a request to " + email + " to be a Caregivee?";
             AlertDialog.Builder builder = new AlertDialog.Builder(Request.this);
             builder.setMessage(alertMessage)
                 .setPositiveButton("Send", (dialog, which) -> {
-                    /* TODO Handling email input here and send request logic ... */
                     // verify email exists
-                    final DatabaseReference ref = database.child("/users");
+                    final DatabaseReference ref = database.child("/users/");
                     ref.orderByChild("email").equalTo(email).addChildEventListener(new ChildEventListener() {
                         @Override
                         public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
                             String caregiveeId = snapshot.getKey();
-                            String role = snapshot.child("/role").getValue().toString(); // confirm caregivee
-                            Log.d("SUCCESS", "onChildAdded: found email with caregivee: " + caregiveeId + " and role: " + role);
+                            String role = snapshot.child("/role").getValue().toString();
+                            Log.d("INFO", "onChildAdded:caregiveeId = " + caregiveeId + ", role = " + role );
+                            // confirm caregivee
                             if(caregiveeId != null && !caregiveeId.isEmpty() && role.equals("caregivee")){
+                                // send request to firebase, display message and transition to dashboard
                                 sendFirebaseRequest(ref,caregiveeId);
-                                // inform user of sent request
-                                String alertReqSent = "Request sent! Please wait for this request to be approved.";
-                                AlertDialog.Builder builderReqSent = new AlertDialog.Builder(Request.this)
-                                        .setMessage(alertReqSent)
-                                        .setPositiveButton("Okay",null);
-                                builderReqSent.show();
-                                transitionToDashboard(v);
+                                displayRequestSent();
+                                Log.i("SUCCESS", "onChildAdded:found email with caregivee: " + caregiveeId + " and role: " + role);
                             } else {
-                                // inform user of sent request
-                                String alertNonCaregivee = "It looks this user is not a registered Caregivee. Please enter a valid Caregivee's email.";
-                                AlertDialog.Builder builderReqSent = new AlertDialog.Builder(Request.this)
-                                        .setMessage(alertNonCaregivee)
-                                        .setPositiveButton("Okay",null);
-                                builderReqSent.show();
+                                displayNonExistingCaregivee();
+                                Log.d("INFO", "onChildAdded:could not add caregivee");
                             }
                         }
 
@@ -124,16 +153,10 @@ public class Request extends AppCompatActivity {
                         public void onChildRemoved(@NonNull DataSnapshot snapshot) { }
                         @Override
                         public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) { }
-
                         @Override
                         public void onCancelled(@NonNull DatabaseError error) {
-                            // display err message
-                            String alertMessage1 = "Cannot find user with email " + email;
-                            AlertDialog.Builder builderErr = new AlertDialog.Builder(Request.this);
-                            builderErr.setMessage(alertMessage1).setNegativeButton("Okay", null);
-                            AlertDialog alertDialog = builder.create();
-                            alertDialog.show();
-                            Log.d("FAIL", "Cannot find such user. Please try again with a valid user email.");
+                            displayNonExistingCaregivee();
+                            Log.d("FAIL", "onChildAdded:" + error.toException());
                         }
                     });
                 }).setNegativeButton("Cancel", null);
