ca6ac10293a22586d2d2fd5af08bb9535978acca
Pratik Naik <pratiknaik@Pratiks-M1-MacBook-Pro.local>
2021-02-18 21:32:00 +0530

    UploadMedia - XML Done

---
+++ app/src/main/java/com/example/caregiver/UploadMedia.java
@@ -0,0 +1,14 @@
+package com.example.caregiver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UploadMedia extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload_media);
+    }
+}
