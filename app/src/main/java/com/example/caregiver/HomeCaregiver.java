19c2a8ca2e6316a126a66e29ef8abb097cc597c1
Minh Le <lenguyetminh1998@gmail.com>
2021-02-19 21:01:00 -0500

    get caregivee

--- app/src/main/java/com/example/caregiver/HomeCaregiver.java
+++ app/src/main/java/com/example/caregiver/HomeCaregiver.java
@@ -1,14 +1,23 @@
 package com.example.caregiver;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ExpandableListView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -25,10 +34,11 @@ public class HomeCaregiver extends Fragment {
     // Set global variables
     ExpandableListView caregiveeList;
     MainAdapter adapter;
-
+    DatabaseReference database = FirebaseDatabase.getInstance().getReference();
+    ArrayList<String> caregiveeNames  = new ArrayList<>();
+    ArrayList<String> caregiveeIds = new ArrayList<>();
 
     public HomeCaregiver() { }
-
 
     public static HomeCaregiver newInstance() {
         HomeCaregiver fragment = new HomeCaregiver();
@@ -40,19 +50,41 @@ public class HomeCaregiver extends Fragment {
         super.onCreate(savedInstanceState);
     }
 
-    public void displayCaregiveeList(){
-        ArrayList<String> listGroup = new ArrayList<>();
-        HashMap<String, ArrayList<String>> listChild = new HashMap<>();
-        listGroup.add("Britney Spears");
-        listGroup.add("Hannah Montana");
-        listGroup.add("Alex Russo");
+    /**
+     * Return list of caregivees associated with the caregiver.
+     */
+    public void queryCaregivees() {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        String userId = preferences.getString("userId", "");
+        DatabaseReference ref = database.child("users/" + userId);
 
-        listGroup.forEach(caregivee -> {
+        ref.addValueEventListener(new ValueEventListener() {@Override
+        public void onDataChange(@NonNull DataSnapshot snapshot) {
+            for (DataSnapshot caregivee :  snapshot.child("caregivees").getChildren()) {
+                String caregiveeId = caregivee.getKey();
+                String caregiveeName = caregivee.getValue().toString();
+                caregiveeNames.add(caregiveeName);
+                caregiveeIds.add(caregiveeId);
+            }
+            displayCaregiveeList();
+        }@Override
+        public void onCancelled(@NonNull DatabaseError error) {
+            Log.d("error", "Can't query caregivees for this caregiver");
+        }
+        });
+    }
+
+    /**
+     * Call after we query all caregivee and display functionality associated with them.
+     */
+    public void displayCaregiveeList(){
+        HashMap<String, ArrayList<String>> listChild = new HashMap<>();
+        caregiveeNames.forEach(caregivee -> {
             ArrayList<String> listChildValues = new ArrayList<String>(
                     Arrays.asList("View Profile", "See Progress", "Set Tasks", "Delete Caregivee"));
             listChild.put(caregivee, listChildValues);
         });
-        adapter = new MainAdapter(listGroup, listChild);
+        adapter = new MainAdapter(caregiveeNames, listChild);
         caregiveeList.setAdapter(adapter);
     }
 
@@ -60,12 +92,13 @@ public class HomeCaregiver extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-
         View view = inflater.inflate(R.layout.fragment_home_caregiver, container, false);
 
+        // Get the caregivee list and display their information
         caregiveeList = (ExpandableListView) view.findViewById(R.id.caregiveeHomelist);
 
-        displayCaregiveeList();
+        // Call firebase to query the caregivees
+        queryCaregivees();
 
         return view;
     }
