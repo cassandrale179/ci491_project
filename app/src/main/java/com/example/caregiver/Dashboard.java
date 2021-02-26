5a3737f13ec4a1b314fcdd3524bf3c6ecd37fef5
minh <lenguyetminh1998@gmail.com>
2021-02-26 13:30:14 -0500

    Fix issue 129

--- app/src/main/java/com/example/caregiver/Dashboard.java
+++ app/src/main/java/com/example/caregiver/Dashboard.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.MenuItem;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -19,13 +20,11 @@ public class Dashboard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        bottomNavigationView = findViewById(R.id.bottomNavigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(bottomNavigationMethod);
 
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
         role = preferences.getString("userRole", "");
-
-        // Set bottom navigation bar
-        bottomNavigationView = findViewById(R.id.bottomNavigation);
-        bottomNavigationView.setOnNavigationItemSelectedListener(bottomNavigationMethod);
 
         if (role.equals("caregiver")) {
             getSupportFragmentManager().beginTransaction().replace(R.id.container, new HomeCaregiver()).commit();
